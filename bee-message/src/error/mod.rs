fd052d7564b245e6bf77bc4e245e4a9d1ba4e14e
Adam-Gleave <adamg108@hotmail.co.uk>
2021-06-30 13:12:48 +0100

    Remove Error wrapper

--- bee-message/src/error/mod.rs
+++ bee-message/src/error/mod.rs
@@ -6,26 +6,3 @@ mod validation;
 
 pub use packable::{MessagePackError, MessageUnpackError};
 pub use validation::ValidationError;
-
-use core::fmt;
-
-#[derive(Debug)]
-pub enum Error {
-    PackError(MessagePackError),
-    UnpackError(MessageUnpackError),
-    ValidationError(ValidationError),
-}
-
-impl_wrapped_variant!(Error, MessagePackError, Error::PackError);
-impl_wrapped_variant!(Error, MessageUnpackError, Error::UnpackError);
-impl_wrapped_variant!(Error, ValidationError, Error::ValidationError);
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::PackError(e) => write!(f, "{}", e),
-            Self::UnpackError(e) => write!(f, "{}", e),
-            Self::ValidationError(e) => write!(f, "Validation error: {}", e),
-        }
-    }
-}
